5e65389595bfc10f8ca353694d53831803728465
Zander Mackie <zmackie@gmail.com>
2017-02-04 14:02:15 -0500

    Parsing json into structs

--- doc_conv/main.go
+++ doc_conv/main.go
@@ -1,9 +1,40 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 )
 
+type Post struct {
+	Body  string `json:"markdown"`
+	Title string `json:"title"`
+	Date  string `json:"created_at"`
+}
+type Posts struct {
+	Posts []Post
+}
+
+var jsonFile = ".blog_posts.json"
+
 func main() {
-	fmt.Println("Hellwo Works")
+
+	// read all the docs and put them each into a struct
+	// - Pull the markdown field
+	// - Pull the title field
+	// - Pull the created at field
+	// output all the structs to markdown files
+	jf, err := ioutil.ReadFile(jsonFile)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(jf)
+
+	var posts Posts
+	err = json.Unmarshal(jf, &posts)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%+v", posts)
+
 }
